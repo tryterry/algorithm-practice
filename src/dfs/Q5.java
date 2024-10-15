8d3d33289a330a9a401dbe3db6dfc6cacef9d0fc
SehyeonLee <909am@neostack.co.kr>
2024-10-15 10:32:36 +0900

    dfs 연습

--- src/dfs/Q5.java
+++ src/dfs/Q5.java
@@ -3,20 +3,25 @@ package dfs;
 import java.util.*;
 import java.io.*;
 
-public class Q3 {
+/**
+ * 백준 1260(실버3)
+ * DFS - 연결된 요소 찾기 유형
+ */
+public class Q5 {
 
-    static int MAX = 100000 + 10;
+    static int MAX = 1000 + 10;
+
     static ArrayList<Integer>[] graph;
     static boolean[] visited;
-    static int N, M, R;
+    static int N, M, V;
     static int[] answer;
     static int order;
-
 
     public static void dfs(int idx) {
         visited[idx] = true;
         answer[idx] = order;
         order++;
+
         for (int i = 0; i < graph[idx].size(); i++) {
             int next = graph[idx].get(i);
             if (visited[next] == false)
@@ -32,16 +37,18 @@ public class Q3 {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
 
-        // 1. graph에 연결 정보 채우기
+        // 1. graph의 연걸 정보 채우기
         graph = new ArrayList[MAX];
-        for (int i = 0; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
+
         visited = new boolean[MAX];
         answer = new int[MAX];
         order = 1;
+        // 간선 채우기
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
@@ -53,16 +60,20 @@ public class Q3 {
         for (int i = 1; i <= N; i++) {
             Collections.sort(graph[i]);
         }
-        // 3. dfs 호출
-        dfs(R);
-        // 3. 출력
 
-        for (int i = 1; i <= N; i++) {
+        // 3. dfs 재귀함수 호출
+
+        dfs(V);
+
+        // 4. 출력
+        for(int i = 1; i <= N; i++ ){
             bw.write(String.valueOf(answer[i]));
             bw.newLine();
         }
+
         br.close();
         bw.close();
 
     }
 }
+
